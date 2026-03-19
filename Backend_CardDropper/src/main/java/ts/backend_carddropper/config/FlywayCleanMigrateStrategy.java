651e077d46da3d1c1880ecbc7d4f493ddae7904f
Andi Ramiqi <andi.ramiqi@hesge.ch>
2026-03-19 19:40:41 +0100

    Fixed flyway strategy migration for prod

--- Backend_CardDropper/src/main/java/ts/backend_carddropper/config/FlywayCleanMigrateStrategy.java
+++ Backend_CardDropper/src/main/java/ts/backend_carddropper/config/FlywayCleanMigrateStrategy.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
+@Profile("!prod")
 public class FlywayCleanMigrateStrategy {
 
     @Bean
